8197f63ae39532072d841006bf2c329c1e31649e
Ahemad <webdeveloper14.intelliworkz@gmail.com>
2026-08-03 16:04:55 +0530

    google tag added again because of extension

--- resources/views/layouts/uk/frontheader-uk.blade.php
+++ resources/views/layouts/uk/frontheader-uk.blade.php
@@ -103,6 +103,19 @@
          inset: 12px auto auto 0px !important;
          }
       </style>
+
+       <!-- Google Tag Manager -->
+      <script>(function (w, d, s, l, i) {
+         w[l] = w[l] || []; w[l].push({
+           'gtm.start':
+             new Date().getTime(), event: 'gtm.js'
+         }); var f = d.getElementsByTagName(s)[0],
+           j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+             'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+         })(window, document, 'script', 'dataLayer', 'GTM-PW87W9HP');
+      </script>
+      <!-- End Google Tag Manager -->
+
       @verbatim
       <script type="application/ld+json">
          {
@@ -152,6 +165,10 @@
       @endverbatim
    </head>
    <body>
+       <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PW87W9HP" height="0" width="0"
+         style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
       <!--    <div id="loader">-->
       <!--       <div class="spinner-border" role="status">-->
       <!--<span class="visually-hidden">Loading...</span>-->
